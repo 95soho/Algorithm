e786039d3f740c5ce942cb848bb4da3cb2897162
cactumer <102403987+cactumer@users.noreply.github.com>
2023-03-04 20:13:27 +0900

    Update 각도기.js

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,3 +1,4 @@
+//내가 한 풀이
 function solution(angle) {
     if (angle > 0 && angle < 90) {
         return 1
@@ -7,5 +8,15 @@ function solution(angle) {
         return 3
     } else if (angle === 180) {
         return 4
+    }
+}
+
+//switch 문을 사용한 다른 좋은 풀이
+function solution(angle) {
+    switch(angle) {
+        case 90: return 2;
+        case 180: return 4;
+        default:
+            return angle > 0 && angle < 90 ? 1 : 3;
     }
 }
