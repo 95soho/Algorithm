7386699256da7ad45dd5659a59184890388b228a
soho <102403987+95soho@users.noreply.github.com>
2023-03-30 20:32:52 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,7 +1,6 @@
 function solution(hp) {
     const a = Math.floor(hp / 5);
-    const b = Math.floor((hp % 5) / 3);
-    const c = (hp % 5) % 3 === 0 ? 0 : Math.floor(((hp % 5) % 3) / 1);
-    var answer = a + b + c;
-    return answer;
+    const b = Math.floor((hp - (a * 5)) / 3);
+    const c = Math.floor(hp - ((a * 5) + (b * 3)) / 1);
+    return a + b + c;
 }
