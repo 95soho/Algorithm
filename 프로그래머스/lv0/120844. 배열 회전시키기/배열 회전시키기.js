3aa822653fcaf7267380e26e408bccf72a1868f4
soho <102403987+95soho@users.noreply.github.com>
2023-04-17 17:24:44 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,5 +1,6 @@
 function solution(numbers, direction) {
-    let left = numbers.filter((n, index) => index !== 0);
-    let right = numbers.filter((n, index) => index !== numbers.length-1);
-    return direction === "left" ? [...left, numbers[0]] : [numbers[numbers.length-1], ...right];
+    direction === "right"
+    ? numbers.unshift(numbers.pop())
+    : numbers.push(numbers.shift())
+    return numbers;
 }
