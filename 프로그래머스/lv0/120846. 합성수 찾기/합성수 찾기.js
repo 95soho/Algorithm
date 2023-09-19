c16dd3427aa7307f36dae523915bdf07a7f98511
Jeongeun Lee <102403987+outoro@users.noreply.github.com>
2023-09-19 16:49:14 +0900

    [level 0] Title: 합성수 찾기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,11 +1,15 @@
-function solution(n) {
-    const arr = [];
-    for(let i = 1; i <= n; i++) {
-        let counter = 0;
-        for(let index = 1; index <= i; index++) {
+function find(i) {
+    let counter = 0;
+    for(let index = 1; index <= i; index++) {
             if(i % index === 0) counter += 1;
         }
-        if(counter >= 3) arr.push(i)
+    return counter;
+}
+
+function solution(n) {
+    let answer = 0;
+    for(let i = 1; i <= n; i++) {
+        if(find(i) >= 3) answer++;
     }
-    return arr.length;
+    return answer;
 }
