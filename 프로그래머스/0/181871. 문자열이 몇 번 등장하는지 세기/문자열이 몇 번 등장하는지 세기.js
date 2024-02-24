03e58317b7207fb7416f69793b7f80c39d91d3e5
outoro <102403987+outoro@users.noreply.github.com>
2024-02-24 15:41:29 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -1,7 +1,7 @@
 function solution(myString, pat) {
     let answer = 0;
     myString.split("").map((str, i) => {
-        if(myString.slice(i).startsWith(pat)) answer += 1;
+        if(myString.slice(i, i + pat.length) === pat) answer += 1;
     })
     return answer;
 }
