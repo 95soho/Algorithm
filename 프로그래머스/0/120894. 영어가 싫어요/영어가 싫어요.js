94580b6b796ef1897d4f6942964d20ad67cee485
Jeonguen Lee <102403987+outoro@users.noreply.github.com>
2024-03-24 21:36:20 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,12 +1,11 @@
 function solution(numbers) {
-    let result = numbers;
     const alphabet = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
     alphabet.forEach(el => {
-        if(result.includes(el)) {
-            result = result.replace(new RegExp(el, 'g'), alphabet.indexOf(el))
+        if(numbers.includes(el)) {
+            numbers = numbers.replace(new RegExp(el, 'g'), alphabet.indexOf(el))
         }
     })
     
-    return Number(result);
+    return Number(numbers);
 }
