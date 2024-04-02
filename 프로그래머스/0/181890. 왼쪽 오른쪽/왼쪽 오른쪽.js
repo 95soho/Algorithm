d047b9203b6bbfc5c166c00289f5b269e0a423f1
Jeonguen Lee <102403987+outoro@users.noreply.github.com>
2024-04-02 18:16:37 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,14 @@
+function solution(str_list) {
+    const l = str_list.indexOf("l")
+    const r = str_list.indexOf("r")
+    
+    if(l === -1 && r === -1) return [];
+    
+    if(l > -1 && r > -1) {
+        if(l < r) return str_list.slice(0, l);
+        else return str_list.slice(r + 1);
+    }
+    
+    if(l > -1) return str_list.slice(0, l)
+    if(r > -1) return str_list.slice(r + 1);
+}
