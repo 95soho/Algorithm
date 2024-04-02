6ab1f4fe5e0fd32947cd8c63686753a5bcf95198
Jeonguen Lee <102403987+outoro@users.noreply.github.com>
2024-04-02 18:31:39 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 02일 18:16:34
+2024년 04월 02일 18:31:38
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -3,12 +3,7 @@ function solution(str_list) {
     const r = str_list.indexOf("r")
     
     if(l === -1 && r === -1) return [];
-    
-    if(l > -1 && r > -1) {
-        if(l < r) return str_list.slice(0, l);
-        else return str_list.slice(r + 1);
-    }
-    
+    if(l > -1 && r > -1) return l < r ? str_list.slice(0, l) : str_list.slice(r + 1);
     if(l > -1) return str_list.slice(0, l)
     if(r > -1) return str_list.slice(r + 1);
 }
