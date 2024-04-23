cdbfc1fd4ef50dace1e4f1379a5b2d9690d9734b
Jeonguen Lee <102403987+outoro@users.noreply.github.com>
2024-04-23 16:58:03 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.97 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    queries.forEach(query => {
+        for(let i = query[0]; i <= query[1]; i++) {
+            if(i % query[2] === 0) arr[i] = arr[i] + 1;
+        }
+    })
+    
+    return arr
+}
