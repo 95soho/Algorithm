3a6e1bb1b78897855b8fd7d321c032ecdd92937c
jeongeun <102403987+zeongeun@users.noreply.github.com>
2024-08-13 15:33:00 +0900

    [level 0] Title: 문자열 밀기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,22 +1,11 @@
 function solution(A, B) {
-    let result = 0;
     if(A === B) return 0;
     
     for(let i = 0; i < A.length; i++) {        
         A = A.split("").map((e, index) => {
-            if(index === 0) {
-                return A[A.length - 1]
-            }
-            return A[index -1];
+            return index === 0 ? A[A.length - 1] : A[index -1]
         }).join("")
-        
-        result += 1;
-        
-        if(A === B) {
-            return result;
-            break;
-        }
+        if(A === B) return i + 1;
     }
-    
     return -1;
 }
