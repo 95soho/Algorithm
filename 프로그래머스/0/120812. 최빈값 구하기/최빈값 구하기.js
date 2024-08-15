8c12f487e630a8e39a9f5032d05b074018a10810
jeongeun <102403987+zeongeun@users.noreply.github.com>
2024-08-15 18:21:35 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,15 +1,13 @@
 function solution(array) {
-    let obj = {}
-        
+    let obj = {};
+
     array.forEach(num => {
-        obj[String(num)] ? obj[String(num)] += 1 : obj[String(num)] = 1;
-    })
-    
-    const arr = Object.values(obj);
-    const max = Math.max(...arr);
-    const count = arr.filter(x => x === max).length;
+        obj[num] = (obj[num] || 0) + 1;
+    });
+
+    const max = Math.max(...Object.values(obj));
+    const count = Object.values(obj).filter(x => x === max).length;
     const result = Object.keys(obj).find(key => obj[key] === max);
-    
+
     return count > 1 ? -1 : +result;
 }
-
