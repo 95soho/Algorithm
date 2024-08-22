0bef7eca08e810620c13695ee533a08832f25f5c
jeongeun <102403987+zeongeun@users.noreply.github.com>
2024-08-22 19:00:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.5 MB, 시간: 5.66 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 22일 18:57:44
+2024년 08월 22일 19:00:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,3 @@
-function getLCM (num1, num2) {
-  let lcm = 1;
-   
-  while(true){
-    if((lcm % num1 == 0) && (lcm % num2 == 0)) break;
-    lcm++;
-  }
-  return lcm
-}
-
 function getGCD(a, b) {
   while(b !== 0) {
     let temp = b;
@@ -19,9 +9,9 @@ function getGCD(a, b) {
 
 
 function solution(numer1, denom1, numer2, denom2) {
-    const lcm = getLCM(denom1, denom2)
-    const numer = numer1 * (lcm / denom1) + numer2 * (lcm / denom2)
-    const gcd = getGCD(numer, lcm);
+    const denom = denom1 * denom2
+    const numer = numer1 * denom2 + numer2 * denom1
+    const gcd = getGCD(numer, denom);
         
-    return [numer / gcd, lcm / gcd];
+    return [numer / gcd, denom / gcd];
 }
