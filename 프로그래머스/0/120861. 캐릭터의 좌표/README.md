37304d6ddea6d9f745010e013b9062f48ae6304b
jeongeun <102403987+zeongeun@users.noreply.github.com>
2024-08-26 20:36:54 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.20 ms
+메모리: 33.5 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 23일 23:28:17
+2024년 08월 26일 20:36:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -2,13 +2,13 @@ function solution(keyinput, board) {
     let result = [0, 0]
     let rangeX = (board[0] - 1) / 2
     let rangeY = (board[1] - 1) / 2
-    
+
     keyinput.map((key) => {
         if(key === "left") result[0] = result[0] - 1;
         if(key === "right") result[0] = result[0] + 1;
         if(key === "up") result[1] = result[1] + 1;
         if(key === "down") result[1] = result[1] - 1;
-                
+
         if (result[0] > rangeX) {
             result[0] = rangeX
         } else if (result[0] < -rangeX) {
@@ -21,6 +21,22 @@ function solution(keyinput, board) {
             result[1] = -rangeY;
         }
     })
-    
+
     return result
+}
+
+function solution(keyinput, board) {
+    let res = [0, 0]
+    let rangeX = (board[0] - 1) / 2
+    let rangeY = (board[1] - 1) / 2
+    
+    for (let k of keyinput) {
+        switch(k){
+            case 'left': if (-res[0] < rangeX) res[0]--; break;
+            case 'right': if (res[0] < rangeX) res[0]++; break;
+            case 'up': if (res[1] < rangeY) res[1]++; break;
+            case 'down': if (-res[1] < rangeY) res[1]--; break;
+        }
+    }
+    return res;
 }
