3442b931942493bbf4608fa0a2590f6b820022b7
jeongeun <102403987+zeongeun@users.noreply.github.com>
2024-09-05 16:10:32 +0900

    [level 0] Title: 안전지대, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/README.md
+++ 프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.32 ms
+메모리: 33.4 MB, 시간: 0.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 05일 15:21:16
+2024년 09월 05일 16:10:28
 
 ### 문제 설명
 
--- 프로그래머스/0/120866. 안전지대/안전지대.js
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -1,35 +1,36 @@
 function solution(board) {
-    let arr = []
+    const n = board.length;
     let result = 0;
     
-    // 지뢰 자표 구하기
-    board.forEach((row, rowIndex) => {
-        row.forEach((value, colIndex) => {
-            if(value === 1) {
-                arr.push([rowIndex, colIndex])
-            }
-        })
-    })
-        
+    // 지뢰의 인접한 좌표를 계산하기 위한 방향 배열 (8방향: 위, 아래, 좌, 우, 대각선)
+    const directions = [
+        [-1, -1], [-1, 0], [-1, 1],  // 위쪽 3칸
+        [0, -1],         [0, 1],      // 좌우 2칸
+        [1, -1], [1, 0], [1, 1]       // 아래쪽 3칸
+    ];
+
     // 위험지역 표시
-    arr.forEach((array) => {
-        let [x, y] = array
-        
-        board.forEach((row, rowIndex) => {
-            if(rowIndex >= x - 1 && rowIndex <= x + 1) {
-                row.forEach((value, colIndex) => {
-                    if(colIndex >= y - 1 && colIndex <= y + 1) {
-                        board[rowIndex][colIndex] = 1
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === 1) {
+                // 지뢰가 있는 곳 주변 8방향에 위험지역 표시
+                directions.forEach(([dx, dy]) => {
+                    const newX = i + dx;
+                    const newY = j + dy;
+                    if (newX >= 0 && newX < n && newY >= 0 && newY < n && board[newX][newY] === 0) {
+                        board[newX][newY] = -1;  // 위험지역을 -1로 표시
                     }
-                })
+                });
             }
-        }) 
-    })
-    
+        }
+    }
+
     // 안전한 지역의 칸수 구하기
     board.forEach(row => {
-        row.forEach(v => v === 0 && result++)
-    })
-    
-    return result
+        row.forEach(v => {
+            if (v === 0) result++;
+        });
+    });
+
+    return result;
 }
